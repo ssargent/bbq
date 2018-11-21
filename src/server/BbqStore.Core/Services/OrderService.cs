db74d10c4189bb2e3f413d873da0c1d0e97be358
Scott Sargent <ssa@dynamicwebusa.com>
2018-11-21 17:30:00 -0500

    docker changes, setting static port for dev

---
+++ src/server/BbqStore.Core/Services/OrderService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BbqStore.Core.Entities;
+using Marten;
+
+namespace BbqStore.Core.Services
+{
+    public interface IOrderService : IEntityService<Order>
+    {
+
+    }
+    public class OrderService : EntityService<Order>, IOrderService
+    {
+        public OrderService(IDocumentSession documentSession) : base(documentSession)
+        {
+        }
+
+
+    }
+}
