848683c248a75f57502a75fe436a1c1fd53694d5
Scott Sargent <ssa@dynamicwebusa.com>
2019-03-02 14:05:41 -0500

    Adding redis to docker run ps1

--- source/go-bbq/run-bbq-docker.ps1
+++ source/go-bbq/run-bbq-docker.ps1
@@ -3,4 +3,4 @@ gci ./secrets | ForEach-Object -Process { $foo = $_.Basename
 	[Environment]::SetEnvironmentVariable($foo, $data, "Process")
 }
 
-docker run -t -i -e BBQ_DB_HOST -e BBQ_DB_NAME -e BBQ_DB_PASSWORD -e BBQ_DB_USER -p 21337:21337 myfamilycooks.azurecr.io/bbq/go-bbq:latest
+docker run -t -i -e BBQ_DB_HOST -e BBQ_DB_NAME -e BBQ_DB_PASSWORD -e BBQ_DB_USER -e BBQ_REDIS_MASTER -e BBQ_REDIS_PASSWORD -p 21337:21337 myfamilycooks.azurecr.io/bbq/go-bbq:latest
