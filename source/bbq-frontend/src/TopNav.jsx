caad19c63193c764a5d8a241dcdbfe460aca130c
Scott Sargent <scott.sargent@gmail.com>
2019-03-04 19:23:34 -0500

    Adding config for frontend service and pod

--- source/bbq-frontend/src/TopNav.jsx
+++ source/bbq-frontend/src/TopNav.jsx
@@ -19,7 +19,7 @@ class TopNav extends Component {
   render() {
     return (
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">BBQ by Mythical Code Labs</a>
+        <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">BBQ <small>by Mythical Code Labs</small></a>
   
   <ul className="navbar-nav px-3">
     <li className="nav-item text-nowrap">
