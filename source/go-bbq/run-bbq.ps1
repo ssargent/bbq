43a4f3936473fbd0133d73a1d1dfdad81506678e
Scott Sargent <ssa@dynamicwebusa.com>
2019-04-24 23:20:43 -0400

    Refactoring where scripts live

--- source/go-bbq/run-bbq.ps1
+++
@@ -1,8 +0,0 @@
-gci ./secrets | ForEach-Object -Process { $foo = $_.Basename
-    $data = gc "./secrets/$_" 
-    [Environment]::SetEnvironmentVariable($foo, $data, "Process")
-}
-
-go build
-
-./go-bbq.exe
